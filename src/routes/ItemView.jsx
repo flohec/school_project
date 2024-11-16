bbbb0ea6dd79846c8139aaae14726893352bf290
Sculptergeist <Sascha.Keus@web.de>
2024-11-17 00:31:42 +0100

    Added new images. Switching color changes now Image Gallery. layout adjustments ItemView.

--- src/routes/ItemView.jsx
+++ src/routes/ItemView.jsx
@@ -2,7 +2,7 @@ import { useParams } from "react-router-dom";
 import Header from "../components/main/Header";
 import Footer from "../components/main/Footer";
 import "../css/itemView.css";
-import { Avatar, Rate } from "antd";
+import { Avatar, Descriptions, Rate } from "antd";
 import { useState } from "react";
 import { ShoppingCartOutlined } from "@ant-design/icons";
 import ReviewCard from "../components/cards/ReviewCard";
@@ -26,8 +26,9 @@ const ItemView = ({ data }) => {
   if (!selectedItem) {
     return <div>Item not found</div>;
   }
-
-  const [mainImage, setMainImage] = useState(selectedItem.subImages[0]);
+  const [imageGallery, setImageGallery] = useState(selectedItem.galleryImages[0])
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const [mainImage, setMainImage] = useState(imageGallery.images[0]);
   const [color, setColor] = useState(selectedItem.colors[0]);
   const [storage, setStorage] = useState(selectedItem.storage[0]);
   const [price, setPrice] = useState(selectedItem.reducedPrice);
@@ -39,6 +40,32 @@ const ItemView = ({ data }) => {
     setPrice2(subStorage.extraPrice + selectedItem.price);
   };
 
+  const handleColorChange = (subColor) => {
+    const newGallery = selectedItem.galleryImages.find(
+      (gallery) => gallery.index === subColor.index
+    );
+    
+    setImageGallery(newGallery);
+
+    let validImageIndex 
+    if(currentImageIndex < newGallery.images.length){
+      validImageIndex = currentImageIndex
+    } else {
+      validImageIndex = newGallery.images.length - 1;
+    }
+    
+    setMainImage(newGallery.images[validImageIndex]);
+    setCurrentImageIndex(validImageIndex)
+  
+    setColor(subColor);
+  };
+
+  const handleImageChange = (subImage, index) => {
+    setMainImage(subImage);
+    setCurrentImageIndex(index);
+  };
+  
+  
   const addToCart = () => {
     console.log("hey");
   };
@@ -47,96 +74,104 @@ const ItemView = ({ data }) => {
     <div className="content">
       <Header sCount={0} cCount={0} data={data} loginDefault={false} />
       <div className="site-content">
-        <div className="item-view">
-          <div className="left-side">
-            <h1 className="item-title">{selectedItem.title}</h1>
-            <div className="rating-container">
-              <Rate
-                allowHalf
-                disabled
-                defaultValue={selectedItem.rating}
-                className="star-rating"
-              />
-              <p className="star-rating-text">{selectedItem.rating + " (63)"} </p>
+        <div className="item-body">
+          <div className="item-image-group">
+            <div className="item-image-gallery">
+              {imageGallery.images.map((subImage, index) => (
+                <div
+                  key={index}
+                  className={`${
+                    mainImage.src === subImage.src ? "active-image" : "sub-image-container"
+                  }`}
+                  onClick={() => handleImageChange(subImage, index)}
+                >
+                  <img
+                    src={subImage.src}
+                    alt={`SubImage ${index + 1}`}
+                    className="sub-image"
+                  />
+                </div>
+              ))}
             </div>
-            <div className="image-group">
-              <div className="item-image-gallery">
-                {selectedItem.subImages.map((subImage, index) => (
+
+            <div className="item-main-image-container">
+              <img className="item-main-image" src={mainImage.src} alt={selectedItem.title} />
+            </div>
+          </div>
+          <div className="item-details">
+            <div className="item-details-button-area">
+              <h1 className="item-title">{selectedItem.title}</h1>
+              <div className="rating-container">
+                <p className="star-rating-text">{selectedItem.rating} </p>
+                <Rate
+                  allowHalf
+                  disabled
+                  defaultValue={selectedItem.rating}
+                  className="star-rating"
+                  />
+                <p className="star-rating-text">(63) ratings</p>
+              </div>
+              <div className="divider"></div>
+
+              <p className="sub-image-title">Color: {color.colorName}</p>
+
+              <div className="sub-images-row">
+                {selectedItem.colors.map((subColor, index) => (
                   <div
-                    key={index}
-                    className={`sub-image-container ${
-                      mainImage.src === subImage.src ? "first-image" : ""
+                  key={index}
+                  className={`choice-button-color ${
+                      color.colorName === subColor.colorName ? "active-choice-button" : ""
                     }`}
-                    onClick={() => setMainImage(subImage)}
+                    onClick={() =>
+                      handleColorChange(subColor)
+                    }
+                    style={{ backgroundColor: subColor.colorHex }}
                   >
-                    <img
-                      src={subImage.src}
-                      alt={`SubImage ${index + 1}`}
-                      className="sub-image"
-                    />
+                    <span className="color-title" style={{ color: subColor.colorText }}>
+                      {subColor.colorName}
+                    </span>
                   </div>
                 ))}
               </div>
 
-              <div className="main-image">
-                <img src={mainImage.src} alt={selectedItem.title} />
-              </div>
-            </div>
-          </div>
+              <p className="sub-image-title">Storage: </p>
 
-          <div className="right-side">
-            <div className="item-description">
-              {selectedItem.itemDescription}
-            </div>
-
-            <p className="sub-image-title">Color: {color.colorName}</p>
-
-            <div className="sub-images-row">
-              {selectedItem.colors.map((subColor, index) => (
-                <div
-                  key={index}
-                  className={`choice-button-color ${
-                    color.colorName === subColor.colorName ? "first-image" : ""
-                  }`}
-                  onClick={() =>
-                    setColor(subColor)
-                  }
-                  style={{ backgroundColor: subColor.colorHex }}
-                >
-                  <span className="color-title" style={{ color: subColor.colorText }}>
-                    {subColor.colorName}
-                  </span>
-                </div>
-              ))}
-            </div>
-
-            <p className="sub-image-title">Storage: </p>
-
-            <div className="sub-storage-row">
-              {selectedItem.storage.map((subStorage, index) => (
-                <div
+              <div className="sub-storage-row">
+                {selectedItem.storage.map((subStorage, index) => (
+                  <div
                   key={index}
                   className={`choice-button-text ${
-                    storage.title === subStorage.title ? "first-image" : ""
-                  }`}
-                  onClick={() => handleStorageClick(subStorage)}
-                >
-                  <p className="storage-title">{subStorage.title}</p>
-                </div>
-              ))}
+                      storage.title === subStorage.title ? "active-choice-button" : ""
+                    }`}
+                    onClick={() => handleStorageClick(subStorage)}
+                    >
+                    <p className="storage-title">{subStorage.title}</p>
+                  </div>
+                ))}
+              </div>
+              <div className="price-row">
+                <p className="canceled-price">{price2.toFixed(2)}€</p>
+                <p className="reduced-price-item">{price.toFixed(2) + "€"}</p>
+              </div>
+              <button
+                className="add-to-cart-button-item hoverable-light"
+                onClick={() => addToCart(selectedItem)}
+              >
+                <ShoppingCartOutlined className="cart-icon" />
+                <span className="button-text">Add to Cart</span>
+              </button>
             </div>
-            <div className="price-row">
-              <p className="canceled-price">{price2.toFixed(2)}€</p>
-              <p className="reduced-price-item">{price.toFixed(2) + "€"}</p>
+            <div className="divider"></div>
+            <div className="item-description">
+              <p className="description-title">About this item</p>
+              <ul>
+                {selectedItem.itemDescription.map((description, index) => (
+                  <li key={index} className="description-list-container">
+                    <div>{description}</div>
+                  </li>
+                ))}
+              </ul>
             </div>
-
-            <button
-              className="add-to-cart-button-item hoverable-light"
-              onClick={() => addToCart(selectedItem)}
-            >
-              <ShoppingCartOutlined className="cart-icon" />
-              <span className="button-text">Add to Cart</span>
-            </button>
           </div>
         </div>
       <ReviewCard />
